fc3b501fd9b01623c2c6fc4574b55985435e1122
Yavor Keskinev <yavor_keskinev@abv.bg>
2019-08-25 16:41:18 +0300

    first build

---
+++ build/precache-manifest.036c8db2d975c49d5c4f0c146656765d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002464848407161ab5cc5a3394b09454",
+    "url": "/index.html"
+  },
+  {
+    "revision": "174808152950d306d8b9",
+    "url": "/static/css/main.a55e5d63.chunk.css"
+  },
+  {
+    "revision": "8127ee7292048170f838",
+    "url": "/static/js/2.85bf0d14.chunk.js"
+  },
+  {
+    "revision": "174808152950d306d8b9",
+    "url": "/static/js/main.5a4deaf2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "16ed2e05cefae0ce5d3ce1b92a606c77",
+    "url": "/static/media/boy1.16ed2e05.svg"
+  },
+  {
+    "revision": "1fac4474e2f4487640fd61030760731c",
+    "url": "/static/media/boy2.1fac4474.svg"
+  },
+  {
+    "revision": "4b8aec94b8ea7391c86b0584b61589b6",
+    "url": "/static/media/boy3.4b8aec94.svg"
+  },
+  {
+    "revision": "fd5ad3951bfbbbc211b68f8112121674",
+    "url": "/static/media/boy4.fd5ad395.svg"
+  },
+  {
+    "revision": "b678b6581df1c08fc38b572a0626a0ca",
+    "url": "/static/media/cloudRainIcon.b678b658.svg"
+  },
+  {
+    "revision": "68f6fe6c309cdce2db2dedcf1b677720",
+    "url": "/static/media/cupBronze.68f6fe6c.svg"
+  },
+  {
+    "revision": "6347c36c500ba07ae492ce6e7d78ba5d",
+    "url": "/static/media/cupGold.6347c36c.svg"
+  },
+  {
+    "revision": "029424808eb97f3cf7e8152f13361e09",
+    "url": "/static/media/cupSilver.02942480.svg"
+  },
+  {
+    "revision": "aa4125d9ec86054f7e30529105c9fba5",
+    "url": "/static/media/empty.aa4125d9.svg"
+  },
+  {
+    "revision": "77c17ebe2853d6442baa9d80015ae1cb",
+    "url": "/static/media/girl1.77c17ebe.svg"
+  },
+  {
+    "revision": "05259861a48b05705c6962ee3085d253",
+    "url": "/static/media/girl2.05259861.svg"
+  },
+  {
+    "revision": "5def5f678f642ecf3dc04e1323cb4298",
+    "url": "/static/media/girl3.5def5f67.svg"
+  },
+  {
+    "revision": "5580008bcc3d952389099a56e715f310",
+    "url": "/static/media/girl4.5580008b.svg"
+  },
+  {
+    "revision": "371c7e98ddc044025d9c253aabe07a17",
+    "url": "/static/media/logo.371c7e98.png"
+  }
+]);

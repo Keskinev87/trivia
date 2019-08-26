3346db87269bf7dae7c28ba422ff36093a9504a7
Yavor Keskinev <yavor_keskinev@abv.bg>
2019-08-26 22:46:53 +0300

    Added logo to the menu

--- src/components/game/ResolveRoundComponent.tsx
+++ src/components/game/ResolveRoundComponent.tsx
@@ -3,6 +3,7 @@ import { PlayerInfo } from '../../reducers/gameReducer';
 import PlayerAvatar from './PlayerAvatar';
 
 export interface ResolveRoundProps {
+    questionType: string | undefined,
     playerInfo: PlayerInfo | undefined;
     opponents: any;
     resolveData: any
@@ -11,6 +12,7 @@ export interface ResolveRoundProps {
  function ResolveRoundComponent(props: ResolveRoundProps) {
     let opponentKeys = Object.keys(props.opponents);
     let playerKey:string;
+    let correctMsg: string;
     props.playerInfo ? playerKey = props.playerInfo.id : playerKey = '123'
     
     let opponentOneProps = {
@@ -28,19 +30,20 @@ export interface ResolveRoundProps {
     if(props.resolveData[opponentKeys[1]].damage > 0)
         opponentTwoProps.damaged = true
 
+    correctMsg = props.questionType === "multiple" ? "Correct answer" : "Closest answer"
     return (
     <div className="resolve-round-container">
         <div className={props.resolveData[playerKey].damage > 0 ? "resolve-player-container wrong" : "resolve-player-container correct"}>
             <PlayerAvatar {...Object.assign({class:'player'}, props.playerInfo, props.resolveData[playerKey].damage > 0 ? {damaged: true} : {damaged: false})} />
-            <div className="player-resolve-status">{props.resolveData[playerKey].damage > 0 ? `Takes ${props.resolveData[playerKey].damage} damage` : "Answered correctly"}</div>
+            <div className="player-resolve-status">{props.resolveData[playerKey].damage > 0 ? `Takes ${props.resolveData[playerKey].damage} damage` : correctMsg}</div>
         </div>
         <div className={props.resolveData[opponentKeys[0]].damage > 0 ? "resolve-player-container wrong" : "resolve-player-container correct"}>
             <PlayerAvatar {...opponentOneProps} />
-            <div className="player-resolve-status">{props.resolveData[opponentKeys[0]].damage > 0 ? `Takes ${props.resolveData[opponentKeys[0]].damage} damage` : "Answered correctly"}</div>
+            <div className="player-resolve-status">{props.resolveData[opponentKeys[0]].damage > 0 ? `Takes ${props.resolveData[opponentKeys[0]].damage} damage` : correctMsg}</div>
         </div>
         <div className={props.resolveData[opponentKeys[1]].damage > 0 ? "resolve-player-container wrong" : "resolve-player-container correct"}>
             <PlayerAvatar {...opponentTwoProps} />
-            <div className="player-resolve-status">{props.resolveData[opponentKeys[1]].damage > 0 ? `Takes ${props.resolveData[opponentKeys[1]].damage} damage` : "Answered correctly"}</div>
+            <div className="player-resolve-status">{props.resolveData[opponentKeys[1]].damage > 0 ? `Takes ${props.resolveData[opponentKeys[1]].damage} damage` : correctMsg}</div>
         </div>
         
     </div>
